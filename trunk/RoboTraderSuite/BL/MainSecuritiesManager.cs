98911ee9a86be93f856864e36cbfc85e0fe7a16c
saharhash <saharhash@952aacaf-f674-4ced-84f9-5b63dc4c49b1>
2015-12-24 21:31:13 +0000

    MainSecuritiesManager skeleton implemented
    git-svn-id: https://subversion.assembla.com/svn/raitch.3@25 952aacaf-f674-4ced-84f9-5b63dc4c49b1

--- trunk/RoboTraderSuite/BL/MainSecuritiesManager.cs
+++ trunk/RoboTraderSuite/BL/MainSecuritiesManager.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using IBApi;
 using Infra.Bus;
 using Infra.Enum;
 using TNS.API.ApiDataObjects;
@@ -18,78 +17,12 @@ namespace TNS.BL
                 case EapiDataTypes.SecurityData:
                     var securityData = message as SecurityData;
 
-                    Contract = securityData.Contract;
-                    LastPrice = securityData.LastPrice;
-                    AskPrice = securityData.AskPrice;
-                    BidPrice = securityData.BidPrice;
-                    HighestPrice = securityData.HighestPrice;
-                    LowestPrice = securityData.LowestPrice;
-                    OpeningPrice = securityData.BasePrice;
-                    AskSize = securityData.BasePrice;
-                    BidSize = securityData.BasePrice;
-                    Volume = securityData.BasePrice;
+                    securities_[securityData.Contract] = securityData;
                     break;
             }
         }
 
-        private Dictionary<Contract, SecurityData> securities_;
-
-        /// <summary>
-        /// Index or underline data (symbol, currency, etc.)
-        /// </summary>
-        public ContractBase Contract { get; set; }
-
-        /// <summary>
-        /// Final price at which the security was traded (current day)
-        /// </summary>
-        public double LastPrice { get; set; }
-
-        /// <summary>
-        /// Highest price made for the security (current day)
-        /// </summary>
-        public double HighestPrice { get; set; }
-
-        /// <summary>
-        /// Lowest price made for the security (current day)
-        /// </summary>
-        public double LowestPrice { get; set; }
-
-        /// <summary>
-        /// Open quote (current day)
-        /// </summary>
-        public double BasePrice { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public double OpeningPrice { get; set; }
-
-        /// <summary>
-        /// Price a seller is willing to accept for a security
-        /// </summary>
-        public double AskPrice { get; set; }
-
-        /// <summary>
-        /// An offer made by an investor, a trader or a dealer to buy a security
-        /// </summary>
-        public double BidPrice { get; set; }
-
-        
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public double AskSize { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public double BidSize { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public double Volume { get; set; }
+        private Dictionary<ContractBase, SecurityData> securities_;
 
     }
 
--- trunk/RoboTraderSuite/TNS.API/ApiDataObjects/SecurityData.cs
+++ trunk/RoboTraderSuite/TNS.API/ApiDataObjects/SecurityData.cs
@@ -12,22 +12,59 @@ namespace TNS.API.ApiDataObjects
     {
 
         public virtual EapiDataTypes APIDataType => EapiDataTypes.SecurityData;
-
+        /// <summary>
+        /// Index or underline data (symbol, currency, etc.)
+        /// </summary>
         public ContractBase Contract { get; set; }
+
+        /// <summary>
+        /// Final price at which the security was traded (current day)
+        /// </summary>
         public double LastPrice { get; set; }
-        public double AskPrice { get; set; }
 
-        public double BidPrice { get; set; }
-
+        /// <summary>
+        /// Highest price made for the security (current day)
+        /// </summary>
         public double HighestPrice { get; set; }
 
+        /// <summary>
+        /// Lowest price made for the security (current day)
+        /// </summary>
         public double LowestPrice { get; set; }
 
+        /// <summary>
+        /// Open quote (current day)
+        /// </summary>
         public double BasePrice { get; set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public double OpeningPrice { get; set; }
+
+        /// <summary>
+        /// Price a seller is willing to accept for a security
+        /// </summary>
+        public double AskPrice { get; set; }
+
+        /// <summary>
+        /// An offer made by an investor, a trader or a dealer to buy a security
+        /// </summary>
+        public double BidPrice { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public double AskSize { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public double BidSize { get; set; }
+
+        /// <summary>
+        /// The number of shares or contracts traded in the security (current day)
+        /// </summary>
         public double Volume { get; set; }
 
         public string Symbol => Contract.Symbol;
