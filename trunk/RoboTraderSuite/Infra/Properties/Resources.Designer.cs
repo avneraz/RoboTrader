8539856d14008108d2c8856037617872101ff927
saharhash <saharhash@952aacaf-f674-4ced-84f9-5b63dc4c49b1>
2015-12-24 19:13:36 +0000

    TNS.Globals rename to infra (infrastructure)
    git-svn-id: https://subversion.assembla.com/svn/raitch.3@19 952aacaf-f674-4ced-84f9-5b63dc4c49b1

--- trunk/RoboTraderSuite/Infra/Properties/Resources.Designer.cs
+++ trunk/RoboTraderSuite/Infra/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TNS.Global.Properties {
+namespace Infra.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace TNS.Global.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TNS.Global.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Infra.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
