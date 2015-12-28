e7dc9e86584ac4ddd9a23c058826043be464ef69
tomraz <tomraz@952aacaf-f674-4ced-84f9-5b63dc4c49b1>
2015-12-28 07:09:17 +0000

    added method for better check if invoke is required
    git-svn-id: https://subversion.assembla.com/svn/raitch.3@39 952aacaf-f674-4ced-84f9-5b63dc4c49b1

--- trunk/RoboTraderSuite/Infra/Extensions/GeneralExtensions.cs
+++ trunk/RoboTraderSuite/Infra/Extensions/GeneralExtensions.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Infra.Extensions
 {
@@ -11,6 +13,20 @@ namespace Infra.Extensions
         public static bool In<T>(this T val, params T[] values) where T : struct
         {
             return values.Contains(val);
+        }
+
+        public static void InvokeIfRequired(this ISynchronizeInvoke obj,
+                                         MethodInvoker action)
+        {
+            if (obj.InvokeRequired)
+            {
+                var args = new object[0];
+                obj.Invoke(action, args);
+            }
+            else
+            {
+                action();
+            }
         }
     }
 }
