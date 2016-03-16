27c07b9e0ae42d61f8726e426d3fbec5b97af832
Tom Raz <tomraz@gmail.com>
2016-03-16 12:26:57 +0200

    Added OptionsRanker

---
+++ RoboTraderSuite/BL/Analysis/OptionsRanker.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infra.Enum;
+using TNS.API.ApiDataObjects;
+
+namespace TNS.BL.Analysis
+{
+    public class RankedOption
+    {
+        public OptionData Option { get; set; }
+        public double Rank { get; set; }
+
+        public RankedOption(OptionData option, double rank)
+        {
+            Option = option;
+            Rank = rank;
+        }
+    }
+    public class OptionsRanker
+    {
+        private readonly double _minimalTheta;
+        private readonly double _minDistanceFromUnlFraction;
+        private readonly int _minExpiryDays;
+        private readonly int _maxExpiryDays;
+        private readonly double SMALL_NUMBER = 0.000001;
+        private readonly double _thetaWeight;
+        private readonly double _vegaWeight;
+        private readonly double _expiryWeight;
+
+        public OptionsRanker(double minimalTheta, double minDistanceFromUNLPercent,
+            int minExpiryDays, int maxExpiryDays,double thetaWeight = 10, 
+            double vegaWeight = 2, double expiryWeight = 1)
+        {
+            _minimalTheta = minimalTheta;
+            _minDistanceFromUnlFraction = minDistanceFromUNLPercent/100.0;
+            _minExpiryDays = minExpiryDays;
+            _maxExpiryDays = maxExpiryDays;
+            _thetaWeight = thetaWeight;
+            _vegaWeight = vegaWeight;
+            _expiryWeight = expiryWeight;
+        }
+
+        public IEnumerable<RankedOption> RankOptions(IEnumerable<OptionData> options)
+        {
+            var filtered = FilterPreconditions(options);
+            return RankOptionsInner(filtered);
+
+        }
+
+        public IEnumerable<OptionData> FilterPreconditions(IEnumerable<OptionData> options)
+        {
+            return options.Where(a => FilterByTheta(a) && FilterByStrike(a) && FilterByExpiry(a));
+        }
+
+        private bool FilterByTheta(OptionData option)
+        {
+            return option.Theta > _minimalTheta;
+        }
+        private bool FilterByStrike(OptionData option)
+        {
+            bool isVialbleCall = option.OptionContract.OptionType == EOptionType.Call && 
+                option.OptionContract.Strike > option.UnderlinePrice*(1 + _minDistanceFromUnlFraction);
+            bool isViablePut = option.OptionContract.OptionType == EOptionType.Put &&
+                option.OptionContract.Strike < option.UnderlinePrice * (1 - _minDistanceFromUnlFraction);
+
+            return isVialbleCall || isViablePut;
+        }
+
+        private bool FilterByExpiry(OptionData optionData)
+        {
+            return optionData.DaysLeft >= _minExpiryDays &&
+                   optionData.DaysLeft <= _maxExpiryDays;
+        }
+
+        private IEnumerable<RankedOption> RankOptionsInner(IEnumerable<OptionData> filtered)
+        {
+            return filtered.Select(o =>
+            {
+                double rank = ThetaRank(o)*_thetaWeight + VegaRank(o)*_vegaWeight + ExpiryRank(o)*_expiryWeight;
+                return new RankedOption(o, rank);
+            }).OrderByDescending(a => a.Rank);
+        }
+
+        private double ThetaRank(OptionData option)
+        {
+            double maxReasonableTheta = 100;
+            double theta = option.Theta;
+            if (theta == 0.0)
+            {
+                theta = SMALL_NUMBER;
+            }
+            return theta / maxReasonableTheta;
+        }
+
+        private double VegaRank(OptionData option)
+        {
+            double maxReasonableVega = 100;
+            double vega = option.Vega;
+            if (vega == 0.0)
+            {
+                vega = SMALL_NUMBER;
+            }
+            return Math.Abs(vega / maxReasonableVega - 1);
+        }
+
+        private double ExpiryRank(OptionData option)
+        {
+            double maxReasonableExpiryDays = 365;
+            return Math.Abs(option.DaysLeft/maxReasonableExpiryDays-1);
+        }
+    }
+
+
+}
---
+++ RoboTraderSuite/BLTests/OptionsRankerTests.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using Infra.Enum;
+using NUnit.Framework;
+using TNS.API.ApiDataObjects;
+using TNS.BL.Analysis;
+
+namespace BLTests
+{
+    [TestFixture]
+    public class OptionsRankerTests
+    {
+        private OptionsRanker _ranker;
+        private const double MINIMAL_THETA = 2;
+        private double MINIMAL_DIS_UNL = 10;
+        private int MIN_EXPIRY_DAYS = 30;
+        private int MAX_EXPIRY_DAYS = 60;
+
+        [SetUp]
+        public void Init()
+        {
+            _ranker = new OptionsRanker(MINIMAL_THETA, MINIMAL_DIS_UNL, MIN_EXPIRY_DAYS, MAX_EXPIRY_DAYS);
+        }
+
+        private OptionData GenerateOptionInPreconditions(EOptionType optionType)
+        {
+            double theta = MINIMAL_THETA + 1;
+            DateTime expiry = DateTime.Now.Add(TimeSpan.FromDays(MIN_EXPIRY_DAYS + 1));
+            double strike;
+            double underline = 100;
+            if (optionType == EOptionType.Call)
+            {
+                strike = underline*(1 + MINIMAL_DIS_UNL/100) + 10;
+            }
+            else
+            {
+                strike = underline*(1 - MINIMAL_DIS_UNL/100) - 10;
+            }
+            var optionContract = new OptionContract("AAPL", expiry, optionType)
+            {
+                Strike = strike
+            };
+            var option = new OptionData()
+            {
+                UnderlinePrice = underline,
+                Theta = theta,
+                OptionContract = optionContract
+            };
+            return option;
+        }
+
+        public void TestPreconditions(List<OptionData> expected,
+            List<OptionData> notExpected)
+        {
+            var options = expected.Concat(notExpected);
+            var res = _ranker.RankOptions(options);
+            Assert.AreEqual(expected.Count(), res.Count());
+            var optionsRes = res.Select(a => a.Option);
+            CollectionAssert.IsSubsetOf(expected, optionsRes);
+            CollectionAssert.IsNotSubsetOf(notExpected, optionsRes);
+        }
+
+        [Test]
+        public void FilterByThetaTest()
+        {
+            var notExpectedObj = GenerateOptionInPreconditions(EOptionType.Call);
+            notExpectedObj.Theta = MINIMAL_THETA - 1;
+            var notExpectedList = new List<OptionData>() {notExpectedObj};
+            var expectedObj = GenerateOptionInPreconditions(EOptionType.Put);
+            expectedObj.Theta = MINIMAL_THETA + 1;
+            var expectedList = new List<OptionData>() { expectedObj };
+            TestPreconditions(expectedList, notExpectedList);
+        }
+
+        [Test]
+        public void FilterByUnlTest()
+        {
+
+            var notExpectedObj = GenerateOptionInPreconditions(EOptionType.Put);
+            notExpectedObj.OptionContract.Strike = 100;
+            notExpectedObj.UnderlinePrice = 10;
+            var notExpectedObj2 = GenerateOptionInPreconditions(EOptionType.Call);
+            notExpectedObj2.OptionContract.Strike = 100;
+            notExpectedObj2.UnderlinePrice = 1000;
+
+            var notExpectedList = new List<OptionData>() { notExpectedObj, notExpectedObj2 };
+            var expectedObj = GenerateOptionInPreconditions(EOptionType.Call);
+            expectedObj.OptionContract.Strike = 100;
+            expectedObj.UnderlinePrice = 10;
+
+            var expectedObj2 = GenerateOptionInPreconditions(EOptionType.Put);
+            expectedObj2.OptionContract.Strike = 10;
+            expectedObj2.UnderlinePrice = 100;
+
+            var expectedList = new List<OptionData>() { expectedObj };
+            TestPreconditions(expectedList, notExpectedList);
+        }
+
+        [Test]
+        public void FilterByExpiryRangeTest()
+        {
+
+            var notExpectedObj = GenerateOptionInPreconditions(EOptionType.Call);
+            notExpectedObj.OptionContract.Expiry = DateTime.Now.Add(TimeSpan.FromDays(MAX_EXPIRY_DAYS + 2));
+            var notExpectedObj2 = GenerateOptionInPreconditions(EOptionType.Call);
+            notExpectedObj2.OptionContract.Expiry = DateTime.Now.Add(TimeSpan.FromDays(MIN_EXPIRY_DAYS - 2));
+            var notExpectedList = new List<OptionData>() { notExpectedObj, notExpectedObj2 };
+            var expectedObj = GenerateOptionInPreconditions(EOptionType.Put);
+            expectedObj.OptionContract.Expiry = DateTime.Now.Add(TimeSpan.FromDays(MIN_EXPIRY_DAYS + 2));
+            var expectedObj2 = GenerateOptionInPreconditions(EOptionType.Put);
+            expectedObj2.OptionContract.Expiry = DateTime.Now.Add(TimeSpan.FromDays(MAX_EXPIRY_DAYS - 2));
+            var expectedList = new List<OptionData>() { expectedObj , expectedObj2 };
+            TestPreconditions(expectedList, notExpectedList);
+        }
+
+        [Test]
+        public void RankTest()
+        {
+            //option1 is the best, after that option2 has higher vega, so it's wrose than 1
+            //and then option3 is the worst, becaue also the expiry is further
+            var option1 = GenerateOptionInPreconditions(EOptionType.Call);
+            option1.Theta = 50;
+            option1.Vega = 0;
+            var option2 = GenerateOptionInPreconditions(EOptionType.Call);
+            option2.Theta = 50;
+            option2.Vega = 2;
+            var option3 = GenerateOptionInPreconditions(EOptionType.Call);
+            option3.Theta = 50;
+            option3.Vega = 2;
+            option3.OptionContract.Expiry = option3.OptionContract.Expiry.Add(TimeSpan.FromDays(1));
+            var res = _ranker.RankOptions(new List<OptionData>() {option3, option2, option1});
+            var optionRes = res.Select(a=> a.Option).ToList();
+            Assert.AreEqual(option1, optionRes[0]);
+            Assert.AreEqual(option2, optionRes[1]);
+            Assert.AreEqual(option3, optionRes[2]);
+
+        }
+
+
+    }
+}
