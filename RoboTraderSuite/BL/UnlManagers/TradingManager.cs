633a236e23734fe9c58e5e98b2129beac430c349
avneraz <avner.raz@gmail.com>
2016-10-19 18:09:49 +0300

    update

--- RoboTraderSuite/BL/UnlManagers/TradingManager.cs
+++ RoboTraderSuite/BL/UnlManagers/TradingManager.cs
@@ -12,9 +12,14 @@ namespace TNS.BL.UnlManagers
     {
         public TradingManager(ITradingApi apiWrapper, ManagedSecurity managedSecurity, UNLManager unlManager) : base(apiWrapper, managedSecurity, unlManager)
         {
+            OrderManager = unlManager.OrdersManager;
         }
 
+        private const int TRADING_CYCLE_TYME = 10;
+        private string _taskId;
+        private IOrdersManager OrderManager { get;  }
         public AccountSummaryData AccountSummaryData { get; set; }
+
         public override bool HandleMessage(IMessage message)
         {
             //ForTest:if(UnlTradingData.Margin > 0){ }
@@ -34,12 +39,14 @@ namespace TNS.BL.UnlManagers
                     switch (eventType)
                     {
                         case ETradingTimeEventType.StartTrading:
+                            _taskId = UNLManager.AddScheduledTaskOnUnl(TimeSpan.FromSeconds(TRADING_CYCLE_TYME), TradingWork, true);
                             break;
                         case ETradingTimeEventType.EndTradingIn30Seconds:
                             break;
                         case ETradingTimeEventType.EndTradingIn60Seconds:
                             break;
                         case ETradingTimeEventType.EndTrading:
+                            UNLManager.RemoveScheduledTaskOnUnl(_taskId);
                             break;
                     }
                     return true;
@@ -47,7 +54,15 @@ namespace TNS.BL.UnlManagers
             return false;
 
         }
-
+        /// <summary>
+        /// Do trading work initiated from scheduler task
+        /// </summary>
+        private void TradingWork()
+        {
+            //Check margin
+            //this.UnlTradingData.MaxAllowedMargin
+            
+        }
         public override void DoWorkAfterConnection()
         {
 
